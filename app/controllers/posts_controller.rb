144c0f1ba235447560eae376b537fb9f4e668203
Davidr1002 <david.rau@kellogg.northwestern.edu>
2022-03-11 16:21:51 +0000

    progress

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,9 @@
 class PostsController < ApplicationController
 
-
   def new
     @post = Post.new
     @post.place_id = params["place_id"]
   end
-
   def create
     if @current_user
       @post = Post.new(params["post"])
